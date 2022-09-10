6e28b0b9634d2ae040ee990ba893f6a89ca25d3d
Paulo Gio <paulinhopereira@ymail.com>
2022-09-09 22:05:43 -0300

    1-9set

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Oi, oi, oi! Sou um contrato e sou inteligente!");
+    }
+}
